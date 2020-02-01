2cc144fb6b43f3d5ca3634011cb458620a64b7b1
TShipley <taneymail@gmail.com>
2020-02-01 22:35:34 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -8,6 +8,12 @@ def hello_t(array)
       yield array[i]
       i = i + 1
     end 
+    
+  array
+ else
+   puts "Hey! No block was given!"
+  end
+end
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
