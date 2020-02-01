0d367d00c11f0713257aa15785f07a7e6e94ae27
TShipley <taneymail@gmail.com>
2020-02-01 22:33:14 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,2 +1,17 @@
+def hello_t
 
+def hello_t(array)
+  if block_given?
+    i = 0
+
+    while i < array.size
+      yield array[i]
+      i = i + 1
+    end 
+
+  array
+  else
+    puts "Hey! No block was given!"
+  end
+end
 
