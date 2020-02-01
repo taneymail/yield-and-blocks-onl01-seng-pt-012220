2826212d24830ad8b62d9b09871e96794e61f8cf
TShipley <taneymail@gmail.com>
2020-02-01 22:26:56 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,11 @@
-def hello_t
+def hello_t(array)
+  if block_given
   1 = 0 
   
   while i < array.length 
     yield array[i]
     i = i + 1 
   end
-end
 
 # call your method here!
 hello_t(["Tim", "Tom", "Jim"]) do |name|
